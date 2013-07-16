508b3a2d3f8065f47b9ab28509e40442583b51e0
Matthew Krueger <mkrueger@Administrators-MacBook-Pro.local>
2013-07-16 13:31:46 -0400

    git is garbage

---
+++ LibraryFiles/Authenticator.h
@@ -0,0 +1,13 @@
+//
+//  Authenticator.h
+//  ArthrexKit
+//
+//  Created by Matthew Krueger on 7/16/13.
+//  Copyright (c) 2013 Arthrex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Authenticator : NSObject
+
+@end
---
+++ LibraryFiles/Authenticator.m
@@ -0,0 +1,13 @@
+//
+//  Authenticator.m
+//  ArthrexKit
+//
+//  Created by Matthew Krueger on 7/16/13.
+//  Copyright (c) 2013 Arthrex. All rights reserved.
+//
+
+#import "Authenticator.h"
+
+@implementation Authenticator
+
+@end
